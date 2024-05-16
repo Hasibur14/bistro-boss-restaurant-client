e1d8875d9c034b015896422c98d6c132354de8e3
hasibur14 <hasib141415@gmail.com>
2024-05-16 11:30:04 +0600

    update featured section

--- src/pages/Home/Featured/Featured.jsx
+++ src/pages/Home/Featured/Featured.jsx
@@ -4,17 +4,24 @@ import './featured.css';
 
 const Featured = () => {
     return (
-        <section className='feature-item'>
-            <SectionTitle subHeading='Check it out' heading='Featured Item'></SectionTitle>
-            <div className='flex py-20 px-32 space-x-10 item-center justify-center'>
-                <div>
-                    <img src={featuredImg} alt="" />
+        <section className='feature-item bg-item bg-fixed rounded-md '>
+            <div className='bg-slate-600 bg-opacity-60 text-white rounded-md'>
+                <div className='p-4'>
+                    <SectionTitle
+                        subHeading='Check it out'
+                        heading='Featured Item'>
+                    </SectionTitle>
                 </div>
-                <div className='space-y-3'>
-                    <p>May 16, 2024</p>
-                    <h3 className='uppercase text-xl font-semibold'>Where cam i get some?</h3>
-                    <p>Intrinsicly deliver functionalized experiences via value-added technologies. Seamlessly build state of the art bandwidth and standards compliant manufactured products. </p>
-                    <button className='btn uppercase bg-transparent text-white border-3 border-b'>Read more</button>
+                <div className='md:flex pt-20 pb-32 px-32 space-x-10 item-center justify-center'>
+                    <div>
+                        <img className='rounded-lg' src={featuredImg} alt="" />
+                    </div>
+                    <div className='space-y-3 justify-center md:mt-28'>
+                        <p>May 16, 2024</p>
+                        <h3 className='uppercase text-xl font-semibold'>Where cam i get some?</h3>
+                        <p>Intrinsicly deliver functionalized experiences via value-added technologies. Seamlessly build state of the art bandwidth and standards compliant manufactured products. </p>
+                        <button className='btn btn-outline border-0 border-b-4 uppercase bg-transparent text-white '>Read more</button>
+                    </div>
                 </div>
             </div>
         </section>
